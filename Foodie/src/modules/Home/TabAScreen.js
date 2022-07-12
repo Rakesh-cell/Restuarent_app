72dd87516b0eaedfa55309b7847229cb74836612
Rakesh-cell <rakeshedu414@gmail.com>
2022-07-12 16:25:44 +0530

    flatlist improvised

--- Foodie/src/modules/Home/TabAScreen.js
+++ Foodie/src/modules/Home/TabAScreen.js
@@ -1,66 +1,100 @@
-import React,{useEffect,useState,useCallback} from 'react';
-import {View,ScrollView, Text,FlatList,Platform} from 'react-native'
+import React, { useEffect, useState, useCallback } from 'react';
+import { View, ScrollView, Text, FlatList, Platform ,StyleSheet} from 'react-native'
 import Card from '../../components/Card';
-import {useDispatch} from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { Fetchdata } from '../../redux/actions';
 
 
-import realm ,{ getAlldata,deleteAlldata } from '../../redux/realmdb';
+import realm, { getAlldata, deleteAlldata } from '../../redux/realmdb';
 
 
 
-  const TabAScreen = ({navigation}) => {
-    const dispatch =useDispatch()
-    const [res,setres]= useState(getAlldata())//to store data 
+const TabAScreen = ({ navigation }) => {
+  const dispatch = useDispatch()
+  const [res, setres] = useState(getAlldata())//to store data 
+  const [refresh,setrefresh] = useState(false)
 
-      useEffect(() => {
-        // if(res.length>4){
-          // deleteAlldata()
-        dispatch(Fetchdata())
+  useEffect(() => {
+    // if(res.length>4){
+    // deleteAlldata()
+    dispatch(Fetchdata())
 
-        // }
-        // setres(getAlldata())
-      },[])
-      Platform.OS === 'android'?console.log("android",getAlldata()):null
+    // }
+    // setres(getAlldata())
+  }, [])
 
-      
- 
-    const ItemSeparatorView = () => {
-      return (
-        <View
-          style={{
-            height: 0.5,
-            width: '100%',
-            backgroundColor: '#f8C8C6',
-            color:'#080707'
-          }}
-        />
-      );
-    };
-    
-
-console.log('res', res.length)
+  const EmptyListMessage = ({item}) => {
     return (
-        <View style={{ flex: 1}}>
-        <FlatList
+      // Flat List Item
+      <Text
+        style={styles.emptyListStyle}
+      >
+        No Data Found, Refresh Again
+      </Text>
+    );
+  };
+
+
+  const ItemSeparatorView = () => {
+    return (
+      <View
+        style={{
+          height: 0.5,
+          width: '100%',
+          backgroundColor: '#f8C8C6',
+          color: '#080707'
+        }}
+      />
+    );
+  };
+  const endComponent = () => {
+    return (
+      <View>
+        <Text style={styles.emptyListStyle}> List ended</Text>
+      </View>
+    );
+  };
+  const onRefresh = async () => {
+    setrefresh(true);
+    await getAlldata();
+    setrefresh(false);
+  };
+ 
+
+  console.log('res', res.length)
+  return (
+    <View style={{ flex: 1 }}>
+      <FlatList
         data={res}
-        showsVerticalScrollIndicator={false}
-        keyExtractor={(item,index)=>index}
+        onRefresh={onRefresh}
+        refreshing={refresh}
         ItemSeparatorComponent={ItemSeparatorView}
-        renderItem={({item}) => {
+        ListEmptyComponent={EmptyListMessage}
+        ListFooterComponent={endComponent}
+        showsVerticalScrollIndicator={false}
+        keyExtractor={(item, index) => index}
+        renderItem={({ item }) => {
           // console.log("inside flat ",item);
           return (
 
-                  <Card item={item}/>
+            <Card item={item} />
 
           );
         }}
-      
-        />
-        
-        {/* <Card/> */}
-      </View>
-    )
-  }
-  
-  export default TabAScreen;
+
+      />
+
+
+    </View>
+  )
+}
+
+export default TabAScreen;
+
+const styles = StyleSheet.create({
+  emptyListStyle: {
+    padding: 5,
+    fontSize: 12,
+    textAlign: 'center',
+  },
+});
